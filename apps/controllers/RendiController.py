2525c19f1a80ce9a670ccd9d623775b01cd9c074
RendiSalim-NEON <rendisalim11@gmail.com>
2022-01-20 16:28:25 +0700

    add CRUD, update still maintain

--- apps/controllers/RendiController.py
+++ apps/controllers/RendiController.py
@@ -1,6 +1,9 @@
+from cmath import log
+from pydantic import ValidationError
+from turtle import update
 from apps.helper import Log
 from apps.schemas import BaseResponse
-from apps.schemas.SchemaCIF import RequestLoanStatus, ResponseLoanStatus
+from apps.schemas.SchemaRendi import ResponseLoanStatus, RequestInsertData, RequestUpdateData
 from apps.helper.ConfigHelper import encoder_app
 from main import PARAMS
 from apps.models.LoanModel import Loan
@@ -10,62 +13,55 @@ SALT = PARAMS.SALT.salt
 
 class ControllerRendi(object):
     @classmethod
-    def get_active_borrower_from_type_and_status_by_rendi(cls, input_data=None):
-        input_data = RequestLoanStatus(**input_data)
+    def read_data_by_status(cls, loan_type, loan_status, limit):
         result = BaseResponse()
         result.status = 400
 
         try:
-            if input_data.limit is None:
-                input_data.limit = Loan.count('loanid')
+            if limit is None:
+                limit = Loan.count('loanid')
 
-            if (input_data.loan_status is not None) and (input_data.loan_type is not None):
+            if (loan_status is not None) and (loan_type is not None):
 
-                if input_data.loan_status not in range (Loan.min('loan_status'), Loan.max('loan_status')+1): 
+                if loan_status not in range (Loan.min('loan_status'), Loan.max('loan_status')+1): 
                     result.message = f"No loan status above {Loan.max('loan_status')} or below {Loan.min('loan_status')}"
                     Log.info(result.message)
 
-                elif input_data.loan_type not in range (Loan.min('loan_type'), Loan.max('loan_type')+1):
+                elif loan_type not in range (Loan.min('loan_type'), Loan.max('loan_type')+1):
                     result.message = f"No loan type above {Loan.max('loan_type')} or below {Loan.min('loan_type')}"
                     Log.info(result.message)
 
                 else:
-                    data = Loan.where('loan_status', '=', input_data.loan_status).where('loan_type', '=', input_data.loan_type).limit(input_data.limit).get().serialize()
+                    data = Loan.where('loan_status', '=', loan_status).where('loan_type', '=', loan_type).limit(limit).get().serialize()
                     result.status = 200
                     result.message = "Success"
                     result.data = ResponseLoanStatus(**{"status_list": data})
                     Log.info(result.message)
 
-            elif input_data.loan_status is not None:
-                if input_data.loan_status not in range (Loan.min('loan_status'), Loan.max('loan_status')+1): 
+            elif loan_status is not None:
+                if loan_status not in range (Loan.min('loan_status'), Loan.max('loan_status')+1): 
                     result.message = f"No loan status above {Loan.max('loan_status')} or below {Loan.min('loan_status')}"
                     Log.info(result.message)
 
                 else:
-                    data = Loan.where('loan_status', '=', input_data.loan_status).limit(input_data.limit).get().serialize()
-                    result.status = 200
-                    result.message = "Success"
-                    result.data = ResponseLoanStatus(**{"status_list": data})
-                    Log.info(result.message)
-
-            elif input_data.loan_type is not None:
-                if input_data.loan_type not in range (Loan.min('loan_type'), Loan.max('loan_type')+1):
-                    result.message = f"No loan type above {Loan.max('loan_type')} or below {Loan.min('loan_type')}"
-                    Log.info(result.message)
-
-                else:
-                    data = Loan.where('loan_type', '=', input_data.loan_type).limit(input_data.limit).get().serialize()
+                    data = Loan.where('loan_status', '=', loan_status).limit(limit).get().serialize()
                     result.status = 200
                     result.message = "Success"
                     result.data = ResponseLoanStatus(**{"status_list": data})
                     Log.info(result.message)
 
             else:
-                data = Loan.limit(input_data.limit).get().serialize()
+                data = Loan.limit(limit).get().serialize()
                 result.status = 200
                 result.message = "Success Query Without Filter"
                 result.data = ResponseLoanStatus(**{"status_list": data})
                 Log.info(result.message)
+
+        except ValidationError:
+            m = 'Wrong data type!'
+            Log.error(m)
+            result.status = 400
+            result.message = str(m)
 
 
         except Exception:
@@ -75,3 +71,95 @@ class ControllerRendi(object):
             result.message = str(m)
 
         return result
+
+    @classmethod
+    def insert_data_by_loanid(cls, input_data=None):
+        input_data = RequestInsertData(**input_data)
+        result = BaseResponse()
+        result.status = 400
+
+        try:
+            if input_data.loanid is not None:
+                Loan.insert(input_data)
+                data = Loan.where('loanid', '=', input_data.loanid).get().serialize()
+                result.status = 200
+                result.message = "Success Input New Data"
+                result.data = ResponseLoanStatus(**{'status_list': data})
+                Log.info(result.message)
+            else:
+                result.status = 404
+                result.message = "We can't insert your data because loanid parameter is empty"
+                Log.info(result.message)
+
+        except:
+            m = "Error"
+            Log.error(m)
+            result.status = 400
+            result.message = str(m)
+
+        return result
+
+    @classmethod
+    def delete_data_by_idno(cls, id_no=None):
+        result = BaseResponse()
+        result.status = 400
+
+        try:
+            if id_no is not None:
+                if id_no in Loan.lists("idno"):
+                    data = Loan.where('idno', '=', id_no)
+                    view = data.get().serialize()
+                    data.delete()
+                    result.status = 200
+                    result.message = f"Success Delete data by id no: {id_no}"
+                    result.data = ResponseLoanStatus(**{'status_list': view})
+                    Log.info(result.message)
+                else:
+                    result.status = 404
+                    result.message = "idno not found"
+            else:
+                result.status = 400
+                result.message = "There's no input"
+                Log.info(result.message)
+        except:
+            m = "Error"
+            Log.error(m)
+            result.status = 400
+            result.message = str(m)
+
+        return result
+
+    @classmethod
+    def update_demography_data(cls, id_no=None, update_data=None):
+        update_data = RequestUpdateData(**update_data)
+        result = BaseResponse()
+        result.status = 400
+
+        try:
+            if id_no is not None:
+                if id_no in Loan.lists("idno"):
+                    Loan.where('idno', id_no).update(update_data)
+                    data = Loan.where('idno', id_no).get().serialize()
+                    result.status = 200
+                    result.message = f"Update phone number by id no: {id_no}"
+                    result.data = ResponseLoanStatus(**{'status_list': data})
+                    Log.info(result.message)
+                else:
+                    result.status = 404
+                    result.message = "idno not found"
+            else:
+                result.status = 400
+                result.message = "There's no input"
+                Log.info(result.message)
+        except:
+            m = "Error"
+            Log.error(m)
+            result.status = 400
+            result.message = str(m)
+
+        return result
+
+
+        
+
+
--- apps/routers/RendiRouter.py
+++ apps/routers/RendiRouter.py
@@ -1,18 +1,69 @@
 import json
-from fastapi import APIRouter, Body, Response
+from optparse import Option
+from unittest import result
+from fastapi import APIRouter, Body, Query, Response
+from typing import Optional
 from apps.controllers.RendiController import ControllerRendi as Rendi
 
 router = APIRouter()
 
-input_loan_status = json.dumps({
-    "loan_status": "1",
+input_create_data = json.dumps({
+    "loanid": None,
     "loan_type": None,
-    "limit": 10
+    "loan_status": None,
+    "loan_amount": None,
+    "loan_tenure": None,
+    "interest": None,
+    "cif": None,
+    "idno": None,
+    "fname": None,
+    "lname": None,
+    "dob": None,
+    "gender": None,
+    "marital_status": None,
+    "income": None,
+    "phone": None,
+    "email": None,
+    "isphoneverified": None,
+    "isemailverified": None,
+    "createdate": None,
+    "updatedate": None,
+    "source": None,
 }, indent = 2)
 
 
-@router.post("/get_active_borrower_from_type_and_status_by_rendi")
-async def get_active_borrower_from_type_and_status_by_rendi(response: Response, input_data=Body(..., example=input_loan_status)):
-    result = Rendi.get_active_borrower_from_type_and_status_by_rendi(input_data=input_data)
+update_data= json.dumps({
+    "fname": 'Rendi',
+    "lname": 'Salim',
+    "dob": "24/05/1998",
+    "gender": "Male",
+    "marital_status": "Single",
+    "income": "150000000",
+    "phone": "111-111-111",
+    "email": 'r.salim@gmail.com'
+})
+
+
+@router.get("/get_borrower_by_status")
+async def read_data_by_status(response: Response, 
+                                                loan_status:int,
+                                                loan_type:Optional[int]=None,
+                                                limit:Optional[int]=None):
+    result = Rendi.read_data_by_status(loan_status=loan_status, loan_type=loan_type, limit=limit)
     response.status_code = result.status
+    return result
+
+@router.post('/append_borrower_data')
+async def insert_data_by_loanid(response: Response, input_data = Body(..., example=input_create_data)):
+    result = Rendi.insert_data_by_loanid(input_data = input_data)
+    return result
+
+@router.delete('/delete_borrower_data')
+async def delete_data_by_idno(response: Response, id_no:Optional[str]=None):
+    result = Rendi.delete_data_by_idno(id_no = id_no)
+    return result
+
+@router.put('/update_borrower_data')
+async def update_demography_data(response: Response, id_no:str, input_data = Body(..., example=update_data)):
+    result = Rendi.update_demography_data(id_no = id_no, update_data = input_data)
     return result
